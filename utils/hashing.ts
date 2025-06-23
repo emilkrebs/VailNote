b78a98e1d39d3a355dd4a2de59cc8ac19522d0f8
Emil Krebs <emil.krebs@outlook.de>
2025-06-23 23:36:26 +0200

    made server hashing synchronous

--- utils/hashing.ts
+++ utils/hashing.ts
@@ -1,8 +1,8 @@
 import * as bcrypt from "bcrypt";
 
-export async function generateHash(password: string): Promise<string> {
-	const salt = await bcrypt.genSalt(8);
-	return await bcrypt.hash(password, salt);
+export function generateHash(password: string): string {
+	const salt = bcrypt.genSaltSync(8);
+	return bcrypt.hashSync(password, salt);
 }
 
 export async function compareHash(
