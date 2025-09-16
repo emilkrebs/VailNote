a9312e69a8ad8f96f5c3be1a184e8a78af4fa1bd
Emil Krebs <emil.krebs@outlook.de>
2025-09-16 19:52:57 +0200

    ignored linting rules

--- lib/rate-limiting/src/arc-rate-limiter.ts
+++ lib/rate-limiting/src/arc-rate-limiter.ts
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-explicit-any
 import { type Context, HttpError, type Middleware } from 'fresh';
 
 import { generateRateLimitHeaders, type RateLimitResult } from './rate-limit-headers.ts';
